140b9a2739eb0bc08db01f1b1d5e6ad0621054f4
Swathy Jayaraj <101557896+swathyjayaraj@users.noreply.github.com>
2023-05-04 12:15:43 +0100

    expander added

--- prediction.py
+++ prediction.py
@@ -50,44 +50,42 @@ if page == 'Data Analysis':
     st.image(image1)
     st.markdown("#### Factors influencing the Price!")
     st.markdown("There are several factors that affect the price of plane tickets. Below are a few of them. Please select the parameter you are interested in learning more about from the list.")
-    choose = st.radio("What factor would you like to explore?",
-    ('Airline','Duration', 'Total Stops', 'Source', 'Destination', 'Journey Date'))
-    if choose == 'Airline':
-        st.markdown("#### Airline")
-        st.markdown('A total of 12 airlines are listed in the dataset. A Jet Airways flight is the most frequent between the cities. Indigo operates the second-highest number of flights. Air India and Spicejet are the other two major airlines in this dataset. We could see that number of premium flights is comparatively lesser than the economical ones thus referring to the purchasing power of the population. ')
-        image3 = Image.open('Aircraft_Pics/Airline.PNG')
-        st.image(image3)
+    st.write("Click on each parameter to explore more")
 
-    if choose == 'Duration':
-        st.markdown("#### Duration")
-        st.markdown('The duration of a flight refers to the length of time it takes to travel from the departure airport to the destination airport. This can vary depending on factors such as distance, weather conditions, and flight route. The price of a flight can vary depending on various factors such as time of booking, demand for the route, and airline policies. In general, longer flights tend to be more expensive than shorter flights, but there are exceptions to this depending on the route and other factors. ')
-        st.markdown('From the below scatter plot there is no clear pattern, as there should have been, regarding the effect of flight duration (or distance) on air ticket prices. We could see a few outlier points in the graph, and this corresponds to other factors like premium airlines and the date of the flight (higher if it occurs on a public holiday).')
-        image4 = Image.open('Aircraft_Pics/Duration.PNG')
-        st.image(image4)
+    with st.expander("Airlines"):
+         st.markdown('A total of 12 airlines are listed in the dataset. A Jet Airways flight is the most frequent between the cities. Indigo operates the second-highest number of flights. Air India and Spicejet are the other two major airlines in this dataset. We could see that number of premium flights is comparatively lesser than the economical ones thus referring to the purchasing power of the population. ')
+         image3 = Image.open('Aircraft_Pics/Airline.PNG')
+         st.image(image3)
 
-    if choose == 'Total Stops':
-        st.markdown("#### Total Stops")
-        st.markdown('A flights price is closely related to its total number of stops. The cost of nonstop flights is generally higher than that of flights with one or more stops. Due to the shorter travel time and comfort of not having to switch planes, non-stop flights are more convenient for passengers willing to pay a premium. However, we see the opposite trend in our dataset. Since the average fare is usually the lowest when compared to flights with multiple stops or layovers, we are essentially paying for one flight for the entire trip. During non-stop flights, fuel is consumed more efficiently, crew and aircraft are utilized better, and ground services are used more efficiently, which reduces airline costs per seat mile.')
-        image5 = Image.open('Aircraft_Pics/Total_stops.PNG')
-        st.image(image5)
+    with st.expander("Duration"):
+         st.markdown('The duration of a flight refers to the length of time it takes to travel from the departure airport to the destination airport. This can vary depending on factors such as distance, weather conditions, and flight route. The price of a flight can vary depending on various factors such as time of booking, demand for the route, and airline policies. In general, longer flights tend to be more expensive than shorter flights, but there are exceptions to this depending on the route and other factors. ')
+         st.markdown('From the below scatter plot there is no clear pattern, as there should have been, regarding the effect of flight duration (or distance) on air ticket prices. We could see a few outlier points in the graph, and this corresponds to other factors like premium airlines and the date of the flight (higher if it occurs on a public holiday).')
+         image4 = Image.open('Aircraft_Pics/Duration.PNG')
+         st.image(image4)
 
-    if choose == 'Source':
-        st.markdown("#### Source")
-        st.markdown("The below plot shows the average airfare from five major Indian cities.It is evident that Bangalore and Delhi have a price that is higher than the mean. There were also a few outliers in both cities, which can be attributed to the Premium Jet services from each city. This may be due to Bangalore's status as the Silicon Valley of India, where the majority of people work in the IT industry and travel home during the holidays. Like Delhi, which is India's capital city, it is a city with a smaller native population and a larger urban population that commutes there for work. This can be further supported by the fact that Bangalore and Delhi have the highest number of IT companies and multinational companies that employ professionals from all over India.")
-        image6 = Image.open('Aircraft_Pics/Source.PNG')
-        st.image(image6)
+    with st.expander("Total Stops"):
+         st.markdown('A flights price is closely related to its total number of stops. The cost of nonstop flights is generally higher than that of flights with one or more stops. Due to the shorter travel time and comfort of not having to switch planes, non-stop flights are more convenient for passengers willing to pay a premium. However, we see the opposite trend in our dataset. Since the average fare is usually the lowest when compared to flights with multiple stops or layovers, we are essentially paying for one flight for the entire trip. During non-stop flights, fuel is consumed more efficiently, crew and aircraft are utilized better, and ground services are used more efficiently, which reduces airline costs per seat mile.')
+         image5 = Image.open('Aircraft_Pics/Total_stops.PNG')
+         st.image(image5)
 
-    if choose == 'Destination':
-        st.markdown("#### Destination")
-        st.markdown("Like the source, the destination can also affect the price of a flight. Vacation spots and major cities may be more expensive than smaller, less popular cities.Several factors may contribute to the high airfare prices in Delhi and New Delhi. As of 2018, jet fuel prices in Delhi increased by 26.4%. Delhi is also the National Capital and seat of government, as well as a popular tourist destination, which could result in higher flight prices. In addition, Bangalore and Cochin are only two of the major airports in south India, a major tourist destination and home to many people in the country.")
-        image7 = Image.open('Aircraft_Pics/Destination.PNG')
-        st.image(image7)
 
-    if choose == 'Journey Date':
-        st.markdown("#### Journey Date")
-        st.markdown("To determine the trend, we extracted months from the Journey date and plotted them against the price. The data shows that the total count of flights is highest in May, and the sum of fare is also maximum during this month. This could be due to summer vacations for schools and colleges, when families tend to travel more. April, on the other hand, has the lowest number of flights, likely due to final exams for schools and colleges. In addition, due to the end of the first quarter, offices are usually busier during April, resulting in a decrease in flight demand. Using this information, we can see how seasonality affects flight availability and pricing.")
-        image8 = Image.open('Aircraft_Pics/Journey.PNG')
-        st.image(image8)
+    with st.expander("Source"):
+         st.markdown("The below plot shows the average airfare from five major Indian cities.It is evident that Bangalore and Delhi have a price that is higher than the mean. There were also a few outliers in both cities, which can be attributed to the Premium Jet services from each city. This may be due to Bangalore's status as the Silicon Valley of India, where the majority of people work in the IT industry and travel home during the holidays. Like Delhi, which is India's capital city, it is a city with a smaller native population and a larger urban population that commutes there for work. This can be further supported by the fact that Bangalore and Delhi have the highest number of IT companies and multinational companies that employ professionals from all over India.")
+         image6 = Image.open('Aircraft_Pics/Source.PNG')
+         st.image(image6)  
+
+    with st.expander("Destination"):
+         st.markdown("Like the source, the destination can also affect the price of a flight. Vacation spots and major cities may be more expensive than smaller, less popular cities.Several factors may contribute to the high airfare prices in Delhi and New Delhi. As of 2018, jet fuel prices in Delhi increased by 26.4%. Delhi is also the National Capital and seat of government, as well as a popular tourist destination, which could result in higher flight prices. In addition, Bangalore and Cochin are only two of the major airports in south India, a major tourist destination and home to many people in the country.")
+         image7 = Image.open('Aircraft_Pics/Destination.PNG')
+         st.image(image7)  
+
+    with st.expander("Journey Date"):
+         st.markdown("To determine the trend, we extracted months from the Journey date and plotted them against the price. The data shows that the total count of flights is highest in May, and the sum of fare is also maximum during this month. This could be due to summer vacations for schools and colleges, when families tend to travel more. April, on the other hand, has the lowest number of flights, likely due to final exams for schools and colleges. In addition, due to the end of the first quarter, offices are usually busier during April, resulting in a decrease in flight demand. Using this information, we can see how seasonality affects flight availability and pricing.")
+         image8 = Image.open('Aircraft_Pics/Journey.PNG')
+         st.image(image8) 
+
+
+
     
 
 
@@ -96,7 +94,7 @@ if page == 'Predict the Price':
     image2 = Image.open('Aircraft_Pics/predict.jpg')
     st.image(image2)
     st.markdown("Select from the below attributes to get an estimate of your flight price.")
-    Airline=st.selectbox('Airline: The name of the airline', ['Jet Airways','IndiGo','Air India','Multiple carriers','SpiceJet','Vistara','Air Asia','GoAir','Multiple carriers Premium economy','Jet Airways Business','Vistara Premium economy','Trujet'])
+    Airline=st.selectbox('Airline: The name of the airline', ['Jet Airways','IndiGo','Air India','Multiple carriers','SpiceJet','Vistara','Air Asia','GoAir'])
     if Airline == "Jet Airways":
         air_inp = 4
     elif Airline == "IndiGo":
